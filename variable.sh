4ea37fe2c4e05e49dff8e77441413da67e8f16df
Shakvilla <abdulshakuraclement@yahoo.com>
2022-09-02 15:30:45 +0000

    added system variables for bash

--- variable.sh
+++ variable.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-name=Abdu-Shakur
+name=Abdu-Shakur # user defined variable
  echo My name is $name
+ # system variables
+ echo the system variable to find current working directory is $PWD
+ echo the system variable to find current working directory is $BASH_VERSION
